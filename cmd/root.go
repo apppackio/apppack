3c66cfc8652acdda5a81b12e475e352f0dd9826d
Peter Baumgartner <pete@lincolnloop.com>
2020-12-11 14:59:48 -0700

    Remove unused config file

--- cmd/root.go
+++ cmd/root.go
@@ -24,9 +24,6 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/briandowns/spinner"
-	homedir "github.com/mitchellh/go-homedir"
-
-	"github.com/spf13/viper"
 )
 
 const ()
@@ -53,43 +50,6 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.apppack.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".apppack" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".apppack")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
 
 func checkErr(err error) {
