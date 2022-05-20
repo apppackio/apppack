9e0bf9453a589ecff5ebabbe8370409141fda34e
Peter Baumgartner <pete@lincolnloop.com>
2022-05-20 16:45:25 -0600

    refactor: move utility functions to new package

---
+++ stringslice/stringslice.go
@@ -0,0 +1,26 @@
+package stringslice
+
+func Contains(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+// deduplicate removes duplicates from a slice of strings
+func Deduplicate(slice []string) ([]string, []string) {
+	seen := make(map[string]bool)
+	var result []string
+	var dupes []string
+	for _, s := range slice {
+		if seen[s] {
+			dupes = append(dupes, s)
+			continue
+		}
+		seen[s] = true
+		result = append(result, s)
+	}
+	return result, dupes
+}
